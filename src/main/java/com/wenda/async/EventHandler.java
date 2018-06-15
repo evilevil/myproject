083ae31243019464a893e3efc1009dbd1c5d7bdb
evilevil <744108017@qq.com>
2018-06-15 14:51:33 +0800

    commit all file

---
+++ src/main/java/com/wenda/async/EventHandler.java
@@ -0,0 +1,10 @@
+package com.wenda.async;
+
+import java.util.List;
+
+
+public interface EventHandler {
+    void doHandle(EventModel model);
+
+    List<EventType> getSupportEventTypes();
+}
---
+++ src/main/java/com/wenda/service/TimelineService.java
@@ -0,0 +1,15 @@
+package com.wenda.service;
+
+import com.wenda.model.Feed;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+
+@Service
+public class TimelineService {
+
+    public List<Feed> pull(int userId, int maxId, int count) {
+        return null;
+    }
+}
